a97ac764a337061ea05d8309eeb04e48c27d8795
kimduyquan0987 <quanhoang0987@gmail.com>
2025-10-02 13:19:43 +0700

    build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -5,6 +5,7 @@ REM Usage: run in folder with wifi_speed.py
 python -m venv venv
 call venv\Scripts\activate
 pip install -r requirements.txt
+pip install speedtest-cli
 pip install pyinstaller
 pyinstaller --onefile --windowed --hidden-import=speedtest --hidden-import=speedtest.cli --collect-submodules speedtest wifi_speed.py
 echo Done. See dist\wifi_speed.exe

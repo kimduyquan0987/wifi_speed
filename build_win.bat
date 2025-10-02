aef6f5cfcba9319186a5e073093762ffd89e8042
kimduyquan0987 <quanhoang0987@gmail.com>
2025-10-02 13:48:51 +0700

    build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -3,6 +3,6 @@ python -m venv venv
 call venv\Scripts\activate
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller --onefile --windowed --hidden-import=speedtest wifi_speed.py
+pyinstaller --onefile --hidden-import speedtest --hidden-import speedtest-cli wifi_speed.py
 echo Done. See dist\wifi_speed.exe
 pause
